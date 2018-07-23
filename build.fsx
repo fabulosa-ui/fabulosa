5bee22c79bee6b29ba2ec2b4414fce0416088f41
Thomas Monte <tbmonte@verisk.com>
2018-07-23 08:04:58 -0600

    adding tests to build scripts

--- build.fsx
+++ build.fsx
@@ -6,6 +6,8 @@ open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
 
+let testDir  = "./tests/"
+
 Target.create "Clean" (fun _ ->
     !! "src/**/bin"
     ++ "src/**/obj"
@@ -17,10 +19,22 @@ Target.create "Build" (fun _ ->
     |> Seq.iter (DotNet.build id)
 )
 
+Target.create "BuildTests" (fun _ ->
+    !! "tests/**/*.*proj"
+    |> Seq.iter (DotNet.build id)
+)
+
+Target.create "Test" (fun _ ->
+    !! "tests/**/*.*proj"
+    |> Seq.iter (DotNet.run id)
+)
+
 Target.create "All" ignore
 
 "Clean"
   ==> "Build"
+  ==> "BuildTest"
+  ==> "Test"
   ==> "All"
 
 Target.runOrDefault "All"
