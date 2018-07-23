afc573be620ec9497fdac119b6d92515a7df3260
Thomas Monte <thomasmont3@gmail.com>
2018-07-22 20:14:04 -0600

    almost there

---
+++ build.fsx
@@ -0,0 +1,220 @@
+#r "paket: groupref netcorebuild //"
+#load ".fake/build.fsx/intellisense.fsx"
+
+#nowarn "52"
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.Globbing.Operators
+open Fake.IO.FileSystemOperators
+open Fake.Tools.Git
+
+module Util =
+
+    let visitFile (visitor: string->string) (fileName : string) =
+        File.ReadAllLines(fileName)
+        |> Array.map (visitor)
+        |> fun lines -> File.WriteAllLines(fileName, lines)
+
+    let replaceLines (replacer: string->Match->string option) (reg: Regex) (fileName: string) =
+        fileName |> visitFile (fun line ->
+            let m = reg.Match(line)
+            if not m.Success
+            then line
+            else
+                match replacer line m with
+                | None -> line
+                | Some newLine -> newLine)
+let platformTool tool =
+    Process.tryFindFileOnPath tool
+    |> function Some t -> t | _ -> failwithf "%s not found" tool
+
+let run (cmd:string) dir args  =
+    if Process.execSimple (fun info ->
+        { info with
+            FileName = cmd
+            WorkingDirectory =
+                if not (String.IsNullOrWhiteSpace dir) then dir else info.WorkingDirectory
+            Arguments = args
+        }
+    ) TimeSpan.MaxValue <> 0 then
+        failwithf "Error while running '%s' with args: %s " cmd args
+
+let npmTool = platformTool "npm"
+
+let npm = run npmTool "./"
+
+Target.create "Clean" (fun _ ->
+    !! "docs/**/bin"
+    ++ "docs/**/obj"
+    ++ "src/**/bin"
+    ++ "src/**/obj"
+    ++ "templates/**/bin"
+    ++ "templates/**/obj"
+    |> Seq.iter Shell.cleanDir
+)
+
+Target.create "Install" (fun _ ->
+    !! "src/**/*.fsproj"
+    |> Seq.iter (fun s ->
+        let dir = IO.Path.GetDirectoryName s
+        DotNet.restore id dir)
+)
+
+Target.create "Build" (fun _ ->
+    !! "src/**/*.fsproj"
+    |> Seq.iter (fun s ->
+        let dir = IO.Path.GetDirectoryName s
+        DotNet.build id dir)
+)
+
+Target.create "QuickBuild" (fun _ ->
+    !! "src/**/*.fsproj"
+    |> Seq.iter (fun s ->
+        let dir = IO.Path.GetDirectoryName s
+        DotNet.build id dir)
+)
+
+Target.create "NpmInstall" (fun _ ->
+    npm "install"
+)
+
+// --------------------------------------------------------------------------------------
+// Docs targets
+
+Target.create "InstallDocs" (fun _ ->
+    !! "docs/**.fsproj"
+    |> Seq.iter(fun s ->
+        let dir = IO.Path.GetDirectoryName s
+        DotNet.restore id dir)
+)
+
+let watchDocs _ =
+    let result =
+        DotNet.exec
+            (DotNet.Options.withWorkingDirectory "docs")
+            "fable"
+            "webpack-dev-server --port free"
+
+    if not result.OK then failwithf "dotnet fable failed with code %i" result.ExitCode
+
+
+Target.create "WatchDocs" watchDocs
+
+Target.create "QuickWatchDocs" watchDocs
+
+Target.create "BuildDocs" (fun _ ->
+    DotNet.exec
+        (DotNet.Options.withWorkingDirectory "docs")
+        "fable"
+        "webpack --port free -- -p"
+    |> ignore
+)
+
+// --------------------------------------------------------------------------------------
+// Build a NuGet package
+let needsPublishing (versionRegex: Regex) (releaseNotes: ReleaseNotes.ReleaseNotes) projFile =
+    printfn "Project: %s" projFile
+    if releaseNotes.NugetVersion.ToUpper().EndsWith("NEXT")
+    then
+        printfn "Version in Release Notes ends with NEXT, don't publish yet."
+        false
+    else
+        File.ReadLines(projFile)
+        |> Seq.tryPick (fun line ->
+            let m = versionRegex.Match(line)
+            if m.Success then Some m else None)
+        |> function
+            | None -> failwith "Couldn't find version in project file"
+            | Some m ->
+                let sameVersion = m.Groups.[1].Value = releaseNotes.NugetVersion
+                if sameVersion then
+                    printfn "Already version %s, no need to publish." releaseNotes.NugetVersion
+                not sameVersion
+
+let toPackageReleaseNotes (notes: string list) =
+    String.Join("\n * ", notes)
+    |> (fun txt -> txt.Replace("\"", "\\\""))
+
+let pushNuget (releaseNotes: ReleaseNotes.ReleaseNotes) (projFile: string) =
+    let versionRegex = Regex("<Version>(.*?)</Version>", RegexOptions.IgnoreCase)
+
+    if needsPublishing versionRegex releaseNotes projFile then
+        let projDir = Path.GetDirectoryName(projFile)
+        let nugetKey =
+            match  Environment.environVarOrNone "NUGET_KEY" with
+            | Some nugetKey -> nugetKey
+            | None -> failwith "The Nuget API key must be set in a NUGET_KEY environmental variable"
+        (versionRegex, projFile)
+        ||> Util.replaceLines (fun line _ ->
+                                    versionRegex.Replace(line, "<Version>"+releaseNotes.NugetVersion+"</Version>") |> Some)
+
+        let result =
+            DotNet.exec
+                (DotNet.Options.withWorkingDirectory projDir)
+                "pack"
+                (sprintf "-c Release /p:PackageReleaseNotes=\"%s\"" (toPackageReleaseNotes releaseNotes.Notes))
+
+        if not result.OK then failwithf "dotnet fable failed with code %i" result.ExitCode
+
+        Directory.GetFiles(projDir </> "bin" </> "Release", "*.nupkg")
+        |> Array.find (fun nupkg -> nupkg.Contains(releaseNotes.NugetVersion))
+        |> (fun nupkg ->
+            Paket.push (fun p -> { p with ApiKey = nugetKey
+                                          WorkingDir = Path.getDirectory nupkg }))
+
+
+Target.create "PublishNugets" (fun _ ->
+    !! "src/Fabulosa/Fabulosa.fsproj"
+    |> Seq.iter(fun s ->
+        let projFile = s
+        let projDir = IO.Path.GetDirectoryName(projFile)
+        let release = projDir </> "RELEASE_NOTES.md" |> ReleaseNotes.load
+        pushNuget release projFile
+    )
+)
+
+// Where to push generated documentation
+let githubLink = "git@github.com:tmonte/Fabulosa.git"
+let publishBranch = "gh-pages"
+let fableRoot   = __SOURCE_DIRECTORY__
+let temp        = fableRoot </> "temp"
+let docsOuput = fableRoot </> "docs" </> "public"
+
+// --------------------------------------------------------------------------------------
+// Release Scripts
+
+Target.create "PublishDocs" (fun _ ->
+  Shell.cleanDir temp
+  Repository.cloneSingleBranch "" githubLink publishBranch temp
+
+  Shell.copyRecursive docsOuput temp true |> Trace.tracefn "%A"
+  Staging.stageAll temp
+  Commit.exec temp (sprintf "Update site (%s)" (DateTime.Now.ToShortDateString()))
+  Branches.push temp
+)
+
+// Build order
+"Clean"
+    ==> "Install"
+    ==> "Build"
+    ==> "PublishNugets"
+
+"Build"
+    ==> "NpmInstall"
+    ==> "InstallDocs"
+    ==> "WatchDocs"
+
+"Build"
+    ==> "NpmInstall"
+    ==> "InstallDocs"
+    ==> "BuildDocs"
+    ==> "PublishDocs"
+
+// start build
+Target.runOrDefault "Build"
