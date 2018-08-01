c6debb34da1bcf584f34e4a3df8abe8c866ec5e1
Thomas Monte <thomasmont3@gmail.com>
2018-08-01 13:36:36 -0600

    better usage

--- src/Fabulosa/ReactAPIExtensions.fs
+++ src/Fabulosa/ReactAPIExtensions.fs
@@ -3,67 +3,31 @@ namespace Fabulosa
 module ReactAPIExtensions =
 
     open Fable.Core
-    open Fable.Core.JsInterop
-
     open Fable.Import.React
     module R = Fable.Helpers.React
 
-    type Children =
-    | Single of ReactElement
-    | Multiple of array<ReactElement>
-
     [<Pojo>]
     type NativeProps = {
-        children: Children option
-        className: string option
+        children: obj option
+        className: string
     }
 
+    [<Pojo>]
     type NativeReactElement = {
         ``type``: string
-        props: NativeProps;
+        props: NativeProps
     }
 
-    let getClasses element =
-        match element.props.className with
-        | Some className -> className
-        | None -> ""
-
-    let getChildren element =
-        match element.props.children with
-        | Some element ->
-            match element with
-            | Single child -> [child]
-            | Multiple children  -> Array.toList children
-        | None -> []
-
-    let extract (element: ReactElement) =
-        let native = unbox<NativeReactElement> element
-        (native.``type``, native.props, getChildren native)
-
-    let optionAppend (text: string) =
+    let getChildren =
         function
-        | Some c -> Some <| c + " " + text
-        | None -> Some text
+        | Some object -> object
+        | None -> [] :> obj
 
+    let extractProps (element: ReactElement) =
+        let native = unbox<NativeReactElement> element
+        (native.``type``, native.props, getChildren <| native.props.children)
 
-    let transform element mapping prop =
-        let (``type``, props, children) = extract element
-        let value = mapping prop
-        
-        Fable.Import.Browser.console.log "original"
-        Fable.Import.Browser.console.log element
-        let appended = props.className |> optionAppend value
-        printf "%O" appended
-               
-        let xx = R.createElement(``type``, {props with className = appended}, props.children)
-                
-        
-        Fable.Import.Browser.console.log "stuff"
-        Fable.Import.Browser.console.log xx
-        
-        xx
-    
-
-
-
-        
+    let transform mapping props element =
+        let (elType, elProps, elChilren) = extractProps element
+        let appended = [elProps.className] @ List.map mapping props |> String.concat " "
+        R.createElement (elType, {elProps with className = appended}, elChilren)        
