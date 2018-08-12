a145d4ba2b70b648927426098e29b996ee90eb57
Thomas Monte <thomasmont3@gmail.com>
2018-08-12 10:32:06 -0600

    initial config for static docs

---
+++ docs/Fabulosa.Docs/pages/button.fsx
@@ -0,0 +1,14 @@
+(**
+## Button
+Buttons include simple button styles for
+actions in different types and sizes.
+*)
+
+(*** include: button-sample ***)
+
+(** 
+### Code
+*)
+
+(*** define: button-sample ***)
+Button.ƒ Button.defaults []
