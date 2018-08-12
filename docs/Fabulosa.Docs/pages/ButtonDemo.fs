004d7c21742954d5f5022883e9d7c7ae79100603
Thomas Monte <thomasmont3@gmail.com>
2018-08-12 17:58:55 -0600

    added button demo

---
+++ docs/Fabulosa.Docs/pages/ButtonDemo.fs
@@ -0,0 +1,24 @@
+﻿module ButtonDemo
+
+open Fabulosa
+module R = Fable.Helpers.React
+
+let button = Button.ƒ Button.defaults [R.str "Default"]
+
+R.mountById "button-default-demo" button
+
+let primary =
+    Button.ƒ {
+        Button.defaults with
+            Kind = Button.Kind.Primary
+    } [R.str "Primary"]
+
+R.mountById "button-primary-demo" primary
+
+let link =
+    Button.ƒ {
+        Button.defaults with
+            Kind = Button.Kind.Link
+    } [R.str "Link"]
+
+R.mountById "button-link-demo" link
--- docs/Fabulosa.Docs/pages/button.fsx
+++ docs/Fabulosa.Docs/pages/button.fsx
@@ -1,14 +1,50 @@
+(*** hide ***)
+#r "../../../src/Fabulosa/bin/Release/netstandard2.0/Fabulosa.dll"
+#load "../../../.paket/load/netstandard2.0/Fable.React.fsx"
+
+open Fabulosa
+open Fable.Import.React
+module R = Fable.Helpers.React
+
+(*** define: button-sample ***)
+let button =
+    Button.ƒ
+        Button.defaults
+        [R.str "Default"]
+
+let primary =
+    Button.ƒ {
+        Button.defaults with
+            Kind = Button.Kind.Primary
+    } [R.str "Primary"]
+
+let link =
+    Button.ƒ {
+        Button.defaults with
+            Kind = Button.Kind.Link
+    } [R.str "Link"]
+
 (**
-## Button
-Buttons include simple button styles for
-actions in different types and sizes.
+<h2 class="s-title">
+    Buttons
+</h2>
+<p class="s-description">
+    Buttons include simple button styles for
+    actions in different types and sizes.
+</p>
+
+### Kinds
+
+Buttons can have the kinds Default, Primary, or Link
+
+<div class="demo">
+    <span id="button-default-demo"></span>
+    <span id="button-primary-demo"></span>
+    <span id="button-link-demo"></span>
+</div>
+
+
+### Code Sample
 *)
 
 (*** include: button-sample ***)
-
-(** 
-### Code
-*)
-
-(*** define: button-sample ***)
-Button.ƒ Button.defaults []
