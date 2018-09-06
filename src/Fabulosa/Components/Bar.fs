77acdc0a4e747fa255254d36b6441fbd23109041
Thomas Monte <thomasmont3@gmail.com>
2018-09-05 22:08:11 -0600

    almost done

--- src/Fabulosa/Components/Bar.fs
+++ src/Fabulosa/Components/Bar.fs
@@ -5,30 +5,71 @@ module Bar =
 
     module R = Fable.Helpers.React
     open R.Props
+    open ClassNames
 
     [<RequireQualifiedAccess>]
     type Props = {
+        Small: bool
         HTMLProps: IHTMLProp list
     }
 
     let defaults = {
+        Props.Small = false
         Props.HTMLProps = []
     }
 
+    let small =
+        function
+        | true -> "bar-sm"
+        | false -> ""
+
     let ƒ (props: Props) =
-        R.div [ClassName "bar"]
+        props.HTMLProps
+        |> addClasses
+            ["bar"
+             small props.Small]
+        |> R.div
 
     [<RequireQualifiedAccess>]
     module Item =
 
         [<RequireQualifiedAccess>]
         type Props = {
+            Value: int
+            Tooltip: bool
             HTMLProps: IHTMLProp list
         }
 
         let defaults = {
+            Props.Value = 0
+            Props.Tooltip = false
             Props.HTMLProps = []
         }
 
+        let private toPercent =
+            string >> (+) >> (|>) "%"
+
+        let private tooltip =
+            function
+            | true -> "tooltip"
+            | false -> ""
+
+        let private tooltipData =
+            function
+            | true, value ->
+                [Data ("tooltip", toPercent value)]
+            | false, _ -> []
+            >> List.cast<IHTMLProp>        
+
+        let private style value =
+            [Style [Width (toPercent value)]]
+            |> List.cast<IHTMLProp>
+           
         let ƒ (props: Props) =
-            R.div [ClassName "bar-item"] []
+            props.HTMLProps
+            @ tooltipData (props.Tooltip, props.Value)
+            @ style props.Value
+            |> addClasses
+                ["bar-item"
+                 tooltip props.Tooltip]
+            |> R.div <| []
--- tests/Fabulosa.Tests/Components/BarTests.fs
+++ tests/Fabulosa.Tests/Components/BarTests.fs
@@ -12,25 +12,64 @@ let tests =
 
         test "Bar default" {
             let item = Bar.Item.ƒ Bar.Item.defaults
-            let bar = Bar.ƒ Bar.defaults [item]
-            
-            bar
+            Bar.ƒ Bar.defaults [item]
             |> ReactNode.unit
             |>! hasUniqueClass "bar"
             |>! hasChild 1 (item |> ReactNode.unit)
             |> hasDescendentClass "bar-item"
         }
 
-        // test "Button custom class" {
-        //     let child = R.div [] [R.str "text"]
-        //     let button =
-        //         Button.ƒ
-        //             { Button.defaults with
-        //                 HTMLProps = [ClassName "custom"] }
-        //             [child]
-            
-        //     button
-        //     |> ReactNode.unit
-        //     |> hasClass "custom"
-        // }
+        test "Bar small" {
+            Bar.ƒ { 
+                Bar.defaults with
+                    Small = true
+            } []
+            |> ReactNode.unit
+            |> hasClass "bar bar-sm"
+        }
+
+        test "Bar html props" {
+            Bar.ƒ {
+                Bar.defaults with 
+                    HTMLProps = [ClassName "custom"]
+            } []
+            |> ReactNode.unit
+            |> hasClass "bar custom"
+        }
+
+        test "Bar item default" {
+            Bar.Item.ƒ Bar.Item.defaults
+            |> ReactNode.unit
+            |> hasUniqueClass "bar-item"
+        }
+
+        test "Bar item html props" {
+            Bar.Item.ƒ {
+                Bar.Item.defaults with
+                    HTMLProps = [ClassName "custom"]
+            }
+            |> ReactNode.unit
+            |> hasClass "bar-item custom"
+        }
+
+        test "Bar item percentage" {
+            Bar.Item.ƒ {
+                Bar.Item.defaults with
+                    Value = 25
+            }
+            |> ReactNode.unit
+            |> hasProp (Style [Width "25%"])
+        }
+
+        test "Bar item tooltip" {
+            Bar.Item.ƒ {
+                Bar.Item.defaults with
+                    Value = 25
+                    Tooltip = true
+            }
+            |> ReactNode.unit
+            |>! hasClass "bar-item tooltip"
+            |>! hasProp (Style [Width "25%"])
+            |> hasProp (Data ("tooltip", "25%"))
+        }
     ]
