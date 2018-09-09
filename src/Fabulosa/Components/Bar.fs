e3225bf21a1dd4feebbfe0c02029842be2166dbd
Thomas Monte <thomasmont3@gmail.com>
2018-09-08 23:32:25 -0600

    bar done

--- src/Fabulosa/Components/Bar.fs
+++ src/Fabulosa/Components/Bar.fs
@@ -61,7 +61,8 @@ module Bar =
         let private style value =
             let width = R.Props.CSSProp.Width (toPercent value)
             let style = Fable.Helpers.React.Props.Style [width]
-            printfn "%A" style
+            printfn "%A" <| style.ToString()
+            printfn "%A" <| style.GetType().ToString()
             [style]
             |> List.cast<IHTMLProp>
 
