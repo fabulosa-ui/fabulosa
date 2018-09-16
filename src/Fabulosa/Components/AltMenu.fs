23446990a08e0df141357cf8e96f6213ec32a2f7
Thomas Monte <thomasmont3@gmail.com>
2018-09-15 20:37:38 -0600

    exploring reactiveCom

---
+++ src/Fabulosa/Components/AltMenu.fs
@@ -0,0 +1,23 @@
+﻿namespace Fabulosa
+
+module AltMenu =
+
+    module R = Fable.Helpers.React
+    open R.Props
+
+    type State =
+        { Opened: bool }
+
+    type Message =
+    | Click
+
+    let init _ = { Opened = false }
+
+    let update message state =
+        match message with
+        | Click -> { state with Opened = not state.Opened }
+
+    let view model dispatch =
+        R.div [] []
+
+    let ƒ () = R.reactiveCom init update view "nil"
